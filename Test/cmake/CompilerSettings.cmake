8cf5bcedbe9e61ca83c6cee81761b321e6d900a2
Rahul Harsha <r4hul.77@gmail.com>
2021-08-05 00:09:44 +0000

    Converted the workspace

--- Test/cmake/CompilerSettings.cmake
+++
@@ -1,5 +0,0 @@
-set(WARNINGS "-Wall") ## -Wextra -Wno-comment -Wno-unused-parameter !!!-Werror
-set(COMMON_FLAGS " ${WARNINGS}")
-
-SET(CMAKE_CXX_FLAGS "${COMMON_FLAGS}  -Wno-register -fno-exceptions -frtti")
-SET(CMAKE_C_FLAGS "${COMMON_FLAGS}")
